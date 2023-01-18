ce45937e332270c8e77eb637290c68e93d5a3ce2
Christopher Friesen <parlance@fifth-harmonic.com>
2023-01-18 13:30:02 -0700

    removed env activation script for linux, impossible with bash apparently

--- linux/env_prompt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ..
-chmod +x ./micromamba/micromamba-`uname`
-eval "$(./micromamba shell hook -s posix --prefix=../env)"
-#./micromamba/micromamba-`uname` shell init --prefix=../env
-./micromamba/micromamba-`uname` activate sd-grpc-server

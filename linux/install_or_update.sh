e13ae934d1713f84c32c2e5bb0be9b4e07f31f6f
Christopher Friesen <parlance@fifth-harmonic.com>
2023-01-19 12:54:57 -0700

    add pip extra index url to linux env update scripts

--- linux/install_or_update.sh
+++ linux/install_or_update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ..
 chmod +x ./micromamba/micromamba-`uname`
+export PIP_EXTRA_INDEX_URL=https://download.pytorch.org/whl/cu116
 [ -d "./env" ] ||  ./micromamba/micromamba-`uname` -r ./env -y create -f ./environment.yaml
 ./micromamba/micromamba-`uname` -r ./env -y update -f ./environment.yaml
 ./micromamba/micromamba-`uname` -r ./env -n sd-grpc-server run python ./scripts/install_or_update.py
